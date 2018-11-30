6b089a48ded66315781ce4bdd73546e6a36c8f64
NIckLee-hanover <42844776+NIckLee-hanover@users.noreply.github.com>
2018-11-30 12:49:00 -0500

    Updated from Brython Server: 11/30/2018 12:49:01 PM

--- Pong.py
+++ Pong.py
@@ -1,1 +1,56 @@
 
+"""
+lines 4-33 copied from lvl 3 for ease.
+
+Pong.py
+Author: 
+Credit: 
+Assignment:
+Write and submit a program that implements the sandbox platformer game:
+https://github.com/HHS-IntroProgramming/Platformer
+"""
+from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset, ImageAsset, Frame
+
+blue = Color(0x2EFEC8, 1.0)
+black = Color(0x000000, 1.0)
+pink = Color(0xFF00FF, 1.0)
+red = Color(0xFF5733, 1.0)
+white = Color(0xFFFFFF, 1.0)
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+white = Color(0xffffff, 1.0)
+grey = Color(0xC0C0C0, 1.0)
+
+thinline = LineStyle(2, black)
+blkline = LineStyle(1, black)
+noline = LineStyle(0, white)
+coolline = LineStyle(1, grey)
+blueline = LineStyle(2, blue)
+redline = LineStyle(1, red)
+greenline = LineStyle(1, green)
+gridline = LineStyle(1, grey)
+grid=RectangleAsset(30,30,gridline,white)
+
+class Pong(App):
+    def __init__(self):
+        super().__init__()
+        bg_main = RectangleAsset(self.width, self.height, noline, black)
+        bg_center =  RectangleAsset(20, self.height-100, noline, white)
+        bg = Sprite(bg_main, (0,0))
+        bg = Sprite(bg_center, (self.width/2, 100))
+
+
+
+
+
+
+
+
+
+
+
+
+myApp = Pong()
+myApp.run()
